3736e4a7613a29d04fbbfeb2ce4cd90f2eacdd0a
Aakarshak Sharma <aakarshak9@outlook.com>
2021-05-04 23:37:33 +0530

    Material Design

--- app/src/main/java/com/example/blooddonorapplication/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/blooddonorapplication/Activities/RegisterActivity.java
@@ -1,32 +1,47 @@
 package com.example.blooddonorapplication.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.blooddonorapplication.R;
+import com.example.blooddonorapplication.Utils.Endpoints;
+import com.example.blooddonorapplication.Utils.VolleySingleton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private TextInputEditText name, city, phone, blood_group, password;
     private Button submit;
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{8,}" +               //at least 8 characters
-                    "$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            //"(?=.*[0-9])" +         //at least 1 digit
+            //"(?=.*[a-z])" +         //at least 1 lower case letter
+            //"(?=.*[A-Z])" +         //at least 1 upper case letter
+            "(?=.*[a-zA-Z])" +      //any letter
+            "(?=.*[@#$%^&+=])" +    //at least 1 special character
+            "(?=\\S+$)" +           //no white spaces
+            ".{8,}" +               //at least 8 characters
+            "$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,103 +54,44 @@ public class RegisterActivity extends AppCompatActivity {
         phone = findViewById(R.id.phone_no);
         blood_group = findViewById(R.id.blood_group);
         password = findViewById(R.id.password);
+        submit= findViewById(R.id.submit);
+
 
     }
 
-    private boolean validateUsername() {
-        String nameM = name.getText().toString().trim();
 
-        if (nameM.isEmpty()) {
-            name.setError("Field Can't Be Empty");
-            return false;
-        } else if (nameM.length() > 20) {
-            name.setError("Username Is Too Long");
-            return false;
-        } else {
-            name.setError(null);
-            return true;
-        }
+
+    private void register(final String name, String city, String phone, String blood_group, String password) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Endpoints.REGISTER_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Success")) {
+                    Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                    RegisterActivity.this.finish();
+                }else {
+                    Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RegisterActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                Log.d("VOLLEY", error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("city", city);
+                params.put("number", phone);
+                params.put("blood_group", blood_group);
+                params.put("password", password);
+                return params;
+            }
+        };
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 
-    private boolean validateCity() {
-        String cityM = city.getText().toString().trim();
-
-        if (cityM.isEmpty()) {
-            city.setError("Field Can't Be Empty");
-            return false;
-        } else if (cityM.length() > 20) {
-            city.setError("City Name is Too Long");
-            return false;
-        } else {
-            city.setError(null);
-            return true;
-        }
-    }
-
-    private boolean validatePhone() {
-        String Phonem = phone.getText().toString().trim();
-
-        if (Phonem.isEmpty()) {
-            phone.setError("Field Can't Be Empty");
-            return false;
-        } else if (Phonem.length() > 10) {
-            phone.setError("Not More Than 10 Digits");
-            return false;
-        } else {
-            phone.setError(null);
-            return true;
-        }
-    }
-
-    private boolean validateBloodGroup() {
-        String blood_groupM = blood_group.getText().toString().trim();
-
-        if (blood_groupM.isEmpty()) {
-            blood_group.setError("Field Cannot Be Empty");
-            return false;
-        } else if (blood_groupM.length() > 3) {
-            blood_group.setError("Only 3 Character Allowed, +/- must Include");
-            return false;
-        } else {
-            blood_group.setError(null);
-            return true;
-        }
-    }
-
-    private boolean validatePassword() {
-
-
-
-        String passwordM = password.getText().toString().trim();
-        if (passwordM.isEmpty()) {
-            password.setError("Field Cannot be Empty");
-            return false;
-        } else if (!PASSWORD_PATTERN.matcher(passwordM).matches()) {
-            password.setError("Must include 0-9, a-z, A-Z, 1 Special Character ");
-            return false;
-        } else if (passwordM.length() < 8) {
-            password.setError("Minimum 8 Character Long");
-            return false;
-        } else {
-            password.setError(null);
-            return true;
-        }
-    }
-
-    public void submit(View view) {
-        if (!validateUsername() | !validateCity() | !validatePhone() | !validateBloodGroup() | !validatePassword()) {
-            return;
-        }
-        String input = "Username: " + name.getText().toString();
-        input += "\n";
-        input += "City: " + city.getText().toString();
-        input += "\n";
-        input += "Phone: " + phone.getText().toString();
-        input += "\n";
-        input += "Blood Group: " + blood_group.getText().toString();
-        input += "\n";
-        input += "Password: " + password.getText().toString();
-
-        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
-    }
 }
