7da791ff9a51b014ae854a95b670fc7955992547
Aakarshak Sharma <aakarshak9@outlook.com>
2021-05-06 02:59:38 +0530

    Home Page

--- app/src/main/java/com/example/blooddonorapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/blooddonorapplication/Activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.blooddonorapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 import com.example.blooddonorapplication.R;
@@ -12,7 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (item.getItemId() == R.id.search_button) {
+                    startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                }
+                return false;
+            }
+        });
     }
 }
