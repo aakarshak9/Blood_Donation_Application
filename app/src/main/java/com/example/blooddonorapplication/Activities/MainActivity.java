b8b0431bdbe7ce0e81a2d75edbfa8e9ce6c8f7d6
Aakarshak Sharma <aakarshak9@outlook.com>
2021-05-08 04:30:37 +0530

    Home Page

--- app/src/main/java/com/example/blooddonorapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/blooddonorapplication/Activities/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.blooddonorapplication.Activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.WindowManager;
 
 import com.example.blooddonorapplication.Adapters.RequestAdapter;
 import com.example.blooddonorapplication.DataModel.RequestDataModel;
@@ -28,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        TextView requestbutton = findViewById(R.id.requestButton);
+        requestbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MakeRequestActivity.class));
+            }
+        });
 
         requestDataModels = new ArrayList<>();
 
--- app/src/main/java/com/example/blooddonorapplication/Activities/MakeRequestActivity.java
+++ app/src/main/java/com/example/blooddonorapplication/Activities/MakeRequestActivity.java
@@ -1,16 +1,89 @@
 package com.example.blooddonorapplication.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.example.blooddonorapplication.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MakeRequestActivity extends AppCompatActivity {
+
+    TextInputEditText messagetext;
+    TextView chooseImageText;
+    ImageView postImage;
+    Button submit_button;
+    Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_request);
+
+        messagetext = findViewById(R.id.message);
+        chooseImageText = findViewById(R.id.choose_text);
+        postImage = findViewById(R.id.post_image);
+        submit_button = findViewById(R.id.submit_button);
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isValid()) {
+                    //Code to Upload Image
+                    uploadRequest(messagetext.getText().toString());
+                }
+            }
+        });
+
+        chooseImageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 101);
+            }
+        });
+
+    }
+
+    private void uploadRequest(String message){
+
+        //Code to upload the Message
+
+    }
+
+    private void uploadImage(){
+
+        //Code to Upload the Image
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101 && resultCode==RESULT_OK){
+            if (data != null) {
+                imageUri = data.getData();
+                Glide.with(getApplicationContext()).load(imageUri).into(postImage);
+            }
+        }
+    }
+
+    private boolean isValid() {
+        if (messagetext.getText().toString().isEmpty()) {
+            messagetext.setError("Field's can't be Empty");
+            return false;
+        }
+        return true;
     }
 }
