5df0e598cedbc8e26a9355d0b61ce45b7414505b
Aakarshak Sharma <aakarshak9@outlook.com>
2021-05-08 05:14:16 +0530

    Home Page

--- app/src/main/java/com/example/blooddonorapplication/Utils/Endpoints.java
+++ app/src/main/java/com/example/blooddonorapplication/Utils/Endpoints.java
@@ -4,4 +4,5 @@ public class Endpoints {
     private static final String BASE_URL = "https://aakarshak.000webhostapp.com/";
     public static final String REGISTER_URL = BASE_URL + "register.php";
     public static final String LOGIN_URL = BASE_URL + "login.php";
+    public static final String UPLOAD_REQUEST = BASE_URL + "";
 }
